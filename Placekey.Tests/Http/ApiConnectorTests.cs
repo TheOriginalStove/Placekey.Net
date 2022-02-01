445d76154f5dd7630d633ff833b7fe6daeafc344
Steven Turturo <steven@ioarmory.com>
2022-02-01 01:59:22 -0700

    chore: set up empty tests for Http Retry Handler

---
+++ Placekey.Tests/Http/ApiConnectorTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Placekey.Api.Http;
+
+namespace Placekey.Api.Tests
+{
+    public class ApiConnectorTests
+    {
+        [Fact(Skip = "Need to set up tests")]
+        public void RetryHandler_IsUsed()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(Skip = "Need to set up tests")]
+        public void RetryHandler_CanRetry()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
