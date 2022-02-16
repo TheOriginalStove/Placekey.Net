cf8ac10307fbecafdedf74f8c72ed7adb1452c87
Steven Turturo <steven@ioarmory.com>
2022-02-15 22:20:21 -0700

    chore: more setup

---
+++ src/Placekey.Api/Authenticators/ApiKeyAuthenticator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Placekey.Core.Guards;
+
+namespace Placekey.Api.Authentication
+{
+    public class ApiKeyAuthenticator : IAuthenticator
+    {
+        public string ApiKey { get; set; }
+
+        public ApiKeyAuthenticator(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public Task Apply(IRequest request, IApiConnector apiConnector)
+        {
+            Guard.Against.Null(request, nameof(request));
+
+
+        }
+
+    }
+
+}
