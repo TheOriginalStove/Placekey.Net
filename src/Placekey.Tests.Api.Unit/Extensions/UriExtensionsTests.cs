31cb34dd4e8b0ba82e2df3445f8fafede3919e0e
Steven Turturo <steven@ioarmory.com>
2022-02-19 03:52:12 -0700

    test: more test setup

---
+++ src/Placekey.Tests.Api.Unit/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Placekey.Tests.Api.Unit.Extensions;
+
+public class UriExtensionsTests
+{
+    [Fact(Skip = "does nothing yet")]
+    public void ApplyParameters_AddsParameters()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+}
