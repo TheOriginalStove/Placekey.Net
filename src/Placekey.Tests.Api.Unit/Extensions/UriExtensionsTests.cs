823484cbb189fbc1170e1974bd800659eeb2e7b5
Steven Turturo <steven@ioarmory.com>
2022-02-19 04:08:10 -0700

    feat: added uri extension test and implementation

--- src/Placekey.Tests.Api.Unit/Extensions/UriExtensionsTests.cs
+++ src/Placekey.Tests.Api.Unit/Extensions/UriExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Placekey.Api.Extensions;
+
 namespace Placekey.Tests.Api.Unit.Extensions;
 
 public class UriExtensionsTests
@@ -7,8 +8,15 @@ public class UriExtensionsTests
     public void ApplyParameters_AddsParameters()
     {
         // Arrange
+        var url = new Uri("https://example.com");
+        var queryParams = new Dictionary<string, string> {{"id", "12345"}};
+
         // Act
+
+        url.ApplyParameters(queryParams);
+
         // Assert
+        url.Query.Should().Contain("id=12345");
     }
 
 }
